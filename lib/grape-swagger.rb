a2f64f42d0009c8c407b1ab9d24ae9edec57684c
Nick Herman <gerkola@gmail.com>
2016-03-21 20:25:40 +0300

    Fix route_param in root namespace failure

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -26,7 +26,7 @@ module Grape
           route_path = route.route_path
           route_match = route_path.split(/^.*?#{route.route_prefix.to_s}/).last
           next unless route_match
-          route_match = route_match.match('\/([\w|-]*?)[\.\/\(]') || route_match.match('\/([\w|-]*)$')
+          route_match = route_match.match('^(?:\/:version)?\/([^/\.\(]*?)([\./\(]|$)')
           next unless route_match
           resource = route_match.captures.first
           next if resource.empty?
